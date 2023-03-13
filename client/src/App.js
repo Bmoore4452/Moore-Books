756270a7dbfdcf7ed11705f49e9925a9a7bf0100
Bmoore4452 <111284578+Bmoore4452@users.noreply.github.com>
2023-03-13 00:52:03 -0400

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route 
-            path='/' 
+            path='/home' 
             element={<SearchBooks />} 
           />
           <Route 
